51d62be6158d3a54b7ab33db0497edd514fc5fe0
Moses Oyet <trickmaxillar@gmail.com>
2023-02-06 17:04:23 +0000

    Add error handler function for 401 request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,17 +2,32 @@
 """
 Route module for the API
 """
+import json
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
+# Define a custome error handler
+@app.Errorhandler(HTTPException)
+def handle_exception(e):
+    """ Return an error for a  HTTP unauthorized request
+    """
+    response = e.get_response()
+    response.data = json.dumps({
+        "code": 401,
+        "error": "Unauthorized"
+        })
+    reesponse.content_type = "application/json"
+    return response
 
 @app.errorhandler(404)
 def not_found(error) -> str:
