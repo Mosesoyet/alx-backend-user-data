e01cdb8cde14d4d1ef8570d9ded73d0e488af36a
Moses Oyet <trickmaxillar@gmail.com>
2023-02-07 18:18:50 +0000

    Create the Auth class to handle authentication

---
+++ 0x01-Basic_authentication/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" The auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    A class to implement authentication
+    """
+    def __init__(self):
+        pass
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Check for authentication on app
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """ Authorization header that returns None
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Check for current user info
+        """
+        return None
