a669883be4542fde62dfcaa08b5a685d709e8352
Moses Oyet <trickmaxillar@gmail.com>
2023-02-07 17:43:08 +0000

    Modify

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,6 +8,8 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 from werkzeug.exceptions import HTTPException
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
