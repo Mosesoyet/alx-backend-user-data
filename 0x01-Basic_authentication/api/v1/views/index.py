d256455a05960b40a77bf32a1bf37cbe03d851b5
Moses Oyet <trickmaxillar@gmail.com>
2023-02-07 17:52:52 +0000

    Modify the view

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,8 +26,8 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> None:
     """ GET /api/vi/unauthorized
     Return error for unauthorized request
     """
