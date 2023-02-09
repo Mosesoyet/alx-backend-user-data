02ba8b43b430a74ba908bdb80d76ed494a7e76ce
Moses Oyet <trickmaxillar@gmail.com>
2023-02-09 03:47:38 +0000

    Fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,10 +2,20 @@
 """
 Basic auth's modules,
 """
-from api.v1.auth.auth import Auth
+import base64
+import re
+from .auth import Auth
 
 
 class BasicAuth(Auth):
     """ Basic Authentication
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ Check the base64 part of the authorization
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?p<token>.+)'
+            f_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
