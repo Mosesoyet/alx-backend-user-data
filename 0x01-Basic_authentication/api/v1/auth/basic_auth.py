77b6c2039af4cfa01c61d4e8e3aa093468c7dafc
Moses Oyet <trickmaxillar@gmail.com>
2023-02-09 03:57:25 +0000

    Add decoder method for b64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic auth's modules,
 """
+import binascii
 import base64
 import re
 from .auth import Auth
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """ The decoded value of base64
+        """
+        if type(base64_authorization_header) == str:
+            try:
+                result = base64.b64decode(
+                        base64_authorization_header,
+                        validate=True,
+                        )
+                return result.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
