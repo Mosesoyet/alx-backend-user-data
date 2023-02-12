2ae147eaf8016b2a13504e55108db8b1345f9865
= <=>
2023-02-12 18:10:40 +0200

    Add is_valid() to check if a password is valid

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """ Hashes a password using random salt
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Checks if password is valid
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
