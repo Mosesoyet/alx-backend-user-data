d1a0e26529260417ea38fc6d3483b10aeb2b0275
= <=>
2023-02-16 07:11:53 -0800

    Modify the return statement for users()

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -4,10 +4,10 @@
 from flask import Flask, jsonify, request
 from auth import Auth
 
-AUTH = Auth()
-
 app = Flask(__name__)
 app.secrete_key = 'random-string'
+
+AUTH = Auth()
 
 
 @app.route('/', strict_slashes=False)
@@ -17,20 +17,16 @@ def index():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=('POST','GET'), strict_slashes=False)
-def users():
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
     """ Register a user
     """
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-
-        try:
-            new_user = AUTH.register_user(email, password)
-            return jsonify({"email": email, "message": "user created"})
-        except:
-            msg = jsonify({"message": "email already registered"})
-            return msg, 400
+    email, password = request.form.get('email'), request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
