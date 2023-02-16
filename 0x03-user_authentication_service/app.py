6949adcf4534312a174ebfc7c484d8859ad1fdfc
= <=>
2023-02-16 06:43:56 -0800

    Add the user class to register a new user in app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """ app's module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 app.secrete_key = 'random-string'
@@ -12,6 +15,22 @@ def index():
     """ The default app page
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=('POST','GET'), strict_slashes=False)
+def users():
+    """ Register a user
+    """
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        try:
+            new_user = AUTH.register_user(email, password)
+            return jsonify({"email": email, "message": "user created"})
+        except:
+            msg = jsonify({"message": "email already registered"})
+            return msg, 400
 
 
 if __name__ == '__main__':
