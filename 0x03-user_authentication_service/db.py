5625da319801dfc9ba2a43a58ec3a3277d2ae633
= <=>
2023-02-16 03:40:43 -0800

    Modify

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" DB module
+"""DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,22 +10,20 @@ from user import Base, User
 
 
 class DB:
-    """ DB class
+    """DB class
     """
 
-
     def __init__(self) -> None:
-        """ Initialize a new DB instance
+        """Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
-
     @property
     def _session(self) -> Session:
-        """ Memoized session object
+        """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -34,13 +32,11 @@ class DB:
 
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Adds a new user to the database.
+        """ Adds a new user to the databae and returns
+        a new User object
         """
-        try:
-            self.user = User(email=self.email, hashed_password=self.hashed_password)
-            self._session.add(self.user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            self.user = None
-        return self.user
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self.session.commit()
+
+        return user
