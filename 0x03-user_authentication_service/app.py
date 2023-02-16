29d08f3730cee4a842e773a9450cf1776caceffc
= <=>
2023-02-16 06:05:49 -0800

    FIx the app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -12,3 +12,9 @@ def index():
     """ The default app page
     """
     return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == '__main__':
+    """ run flask app
+    """
+    app.run(host='0.0.0.0', port="5000")
