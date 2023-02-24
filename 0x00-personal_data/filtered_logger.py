7d709d60831394e47ff14bceeecf8dff50ccd072
= <=>
2023-02-24 01:53:59 -0800

    Add PII_FILEDS

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,19 +7,15 @@ import logging
 from typing import List
 import mysql.connector
 
-patterns = {
-    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
-    'replace': lambda x: r'\g<field>={}'.format(x),
-}
-
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """Filters a log line.
     """
-    extract, replace = (patterns["extract"], patterns["replace"])
-    return re.sub(extract(fields, separator), replace(redaction), message)
+    return (separator.join(x if x.split('=')[0] not in fields else re.sub(
+        r'=.*', '=' + redaction, x) for x in message.split(separator)))
 
 
 def get_logger() -> logging.Logger:
